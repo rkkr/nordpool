ce2f875884e9584b413870c1ec5e8c18c5643371
Raimondas Rimkus <3480123+rkkr@users.noreply.github.com>
2024-11-28 19:02:47 +0200

    str format

--- crawler/nordpool.py
+++ crawler/nordpool.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 
-API_URL = 'https://dataportal-api.nordpoolgroup.com/api/DayAheadPrices?market=DayAhead&deliveryArea=LT&currency=EUR&date='
+API_URL = 'https://dataportal-api.nordpoolgroup.com/api/DayAheadPrices?date=%s&market=DayAhead&deliveryArea=LT&currency=EUR'
 TIMEOUT = 60
 
 def download(date: datetime):
@@ -10,14 +10,16 @@ def download(date: datetime):
     return data
 
 def _fetch_json(date: datetime):
-    url = API_URL + date.strftime('%Y-%m-%d')
+    url = API_URL % date.strftime('%Y-%m-%d')
     response = requests.get(url, timeout=TIMEOUT)
     if response.status_code == 200:
         return response.json()
     else:
-        return []
+        return None
 
 def _parse_json(json):
+    if json is None:
+        return None
     _hour_prices = []
 
     for row in json['multiAreaEntries']:
