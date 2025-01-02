9a76dc4bf134c949ff758e46429bc3bb4e29be55
rkkr <narimantukas@gmail.com>
2025-01-02 09:17:19 +0200

    ESO downozour

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,9 +2,7 @@ import nordpool, taxes, cache
 import os, logging
 from datetime import datetime, timedelta, timezone
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-logging.basicConfig(filename=os.path.join(os.path.dirname(DIR_PATH), 'log.txt'), filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename='/var/log/nordpool', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 if __name__ == "__main__":
     try:
